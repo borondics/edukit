d37fe6166c0eb4dcab25625bd3027f801cc33f31
borondics <borondics.accounts@gmail.com>
2019-05-14 14:43:53 +0200

    fixes to code and text

--- randomWord.js
+++ randomWord.js
@@ -2,7 +2,6 @@ function loadText() {
 	var stringAllText = JSON.stringify(allText);
 	var myText = JSON.parse(stringAllText);
 	var s = myText[0].content;
-//	console.log(s);
 
 	document.getElementById('changeButton').focus();
 	
@@ -10,25 +9,22 @@ function loadText() {
 }
 
 function displayRandomWord() {
-//	var ss = "Unité 7 - Lecture 6 1. Lis ces mots : chaque - pourtant - vraiment - surtout jamais - désormais -aussitôt - enfin 2. Lis ce texte : Désormais sur la planète Cacahouète réfléchit. Elle sort une graine de sa poche la dépose sur le sol. Rien ne se passe et Bouchon s'énerve et s'agite. Tiptaptop. Cacahouète demande à son ordinateur : Que faut-il pour faire germer des graines ? - De la terre répond l'ordinateur. Alors. Cacahouete consulte ses recettes magiques. Elle rassemble les produits nécessaires dans son chaudron. Puis elle le dépose délicatement sur le sol et prononce la formule magique. Aussitôtle mélange frémit et le chaudron malacticocorico roule de tous les côtés. Soudain quelque chose gronde et un bruit énorme retentit. C'est un volcan! De la lave et des roches brûlantes se répandent sur le sol. Trouve vite une formule supplie Bouchon qui entraine Cacahouète à l'abri dans la capsule. La sorcière tremble de tout son corps. Impossible de réfléchir... La lave s'approche de la capsule. Sauve qui peut ! hurle Bouchon. Les sorcieres et les cochons d'abord! ";
 	var s = loadText();
 	
 	var sentences = s.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")
+	var sentenceItem = sentences[Math.floor(Math.random()*sentences.length)];
 	
 	var punctuationless = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?0-9«<>≪≫]/g,"");
 	var finalString = punctuationless.replace(/\s{2,}/g," ");
-	
 	var wordItems = finalString.split(' ');
 	var wordItem = wordItems[Math.floor(Math.random()*wordItems.length)];
 
-	var sentenceItem = sentences[Math.floor(Math.random()*sentences.length)];
 
-//add color change
+	//TODO: add color change of text on button press
 	if (document.getElementById("sentence").checked == true) {
 		document.getElementById("changeButton").innerHTML = sentenceItem;
 	  } else {
 		document.getElementById("changeButton").innerHTML = wordItem.toLowerCase();
 	  }
- 
  
 }
